7e3977649669f4336ee5d1a5dc47b578b7f10ecf
MOTDF2 <gregor_baer@gmx.ch>
2026-08-11 10:28:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // Verbindung auf den Cache zurückfallen). Reine Assets (Icons) bleiben
 // Cache-First, da sie sich praktisch nie ändern. So kommen Updates zuverlässig
 // an, auch wenn die App bereits als Homescreen-Icon installiert ist.
-const CACHE_NAME = 'muenzsammlung-v15';
+const CACHE_NAME = 'muenzsammlung-v16';
 const NETWORK_FIRST = ['./', './index.html', './manifest.json'];
 const ASSETS = [
   './',
